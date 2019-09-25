c9423da17b4fe1bebcc000753e18f9024e8e25d3
alecphobia95 <alecphobia95@gmail.com>
2019-09-25 17:33:23 -0400

    Update GridMoveScript.cs

--- JumpyStreetProject/Assets/Scripts/GridMoveScript.cs
+++ JumpyStreetProject/Assets/Scripts/GridMoveScript.cs
@@ -4,22 +4,23 @@ using UnityEngine;
 
 public class GridMoveScript : MonoBehaviour
 {
-    //Rotation direction for x needs to be minus 90 since the model is not rotated
+    //gets relative directions
     private Vector3 up = Vector3.zero,
         right = new Vector3(0, 90, 0),
         down = new Vector3(0, 180, 0),
         left = new Vector3(0, 270, 0),
         currentDir = Vector3.zero;
 
+    //to be filled in to see which way to face and which way to go
     private Vector3 nextPos, destination, diection;
 
     private float speed = 4f;
+    private float rayLength = 1f;
 
     private bool canMove;
 
-    private float rayLength = 1f;
+    
 
-    // Start is called before the first frame update
     void Start()
     {
         currentDir = up;
@@ -27,17 +28,48 @@ public class GridMoveScript : MonoBehaviour
         destination = transform.position;
     }
 
-    // Update is called once per frame
     void Update()
     {
         Move();
     }
 
+    //Handles where you gonna move
     private void Move()
     {
         transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
 
-        if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        GetInputs();
+
+        if (Vector3.Distance(destination, transform.position) <= .0001f)
+        {
+            transform.localEulerAngles = currentDir;
+            //If are not stuck and can move to a spot will move you
+            if (canMove && Valid())
+            {
+                //Only corrects your horizontal placement if you are moving up our down
+                //This allows us to avoid needing nodes to place player on while keeping
+                //the player from crashing into a wall they are halfway through
+                if (currentDir == up || currentDir == down)
+                {
+                    destination = transform.position + nextPos;
+                    Mathf.Round(destination.x);
+                    canMove = false;
+                }
+                else
+                {
+                    destination = transform.position + nextPos;
+
+                    canMove = false;
+                }
+            }
+            
+        }
+    }
+
+    //Gets your inputs
+    private void GetInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
         {
             nextPos = Vector3.forward;
             currentDir = up;
@@ -46,7 +78,6 @@ public class GridMoveScript : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
         {
             nextPos = Vector3.back;
-            destination.x = Rounded(destination.x);
             currentDir = down;
             canMove = true;
         }
@@ -62,29 +93,9 @@ public class GridMoveScript : MonoBehaviour
             currentDir = left;
             canMove = true;
         }
-
-        if (Vector3.Distance(destination, transform.position) <= .0001f)
-        {
-            transform.localEulerAngles = currentDir;
-            if (canMove && Valid())
-            {
-                if (currentDir == up || currentDir == down)
-                {
-                    destination = transform.position + nextPos;
-                    destination.x = Rounded(destination.x);
-                    canMove = false;
-                }
-                else
-                {
-                    destination = transform.position + nextPos;
-
-                    canMove = false;
-                }
-            }
-            
-        }
     }
 
+    //Checks to see if you can move there
     private bool Valid()
     {
         Ray myRay = new Ray(transform.position + new Vector3(0, .25f, 0), transform.forward);
@@ -100,12 +111,6 @@ public class GridMoveScript : MonoBehaviour
             }
         }
         return true;
-    }
-
-    private float Rounded(float target)
-    {
-        target = Mathf.Round(target);
-        return target;
     }
 
 }
