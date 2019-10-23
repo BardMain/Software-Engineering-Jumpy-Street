13760fa60c29a2f55f2e57d927f403177cf8c63e
alecphobia95 <46802114+alecphobia95@users.noreply.github.com>
2019-10-23 14:54:46 -0400

    Backup in case
    made backup scene and scripts just in case

---
+++ JumpyStreetProject/Assets/Scripts/ObstacleSpawnBackupScript.cs
@@ -0,0 +1,235 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleSpawnBackupScript : MonoBehaviour
+{
+    public static ObstacleSpawnBackupScript instance;
+
+    public GameObject[] roadObstacles = new GameObject[1];
+    private List<float> rdDirections = new List<float>();
+    private List<GameObject> spawnedRdObj = new List<GameObject>();
+    private GameObject[] rdSpawns = new GameObject[0];
+
+    public GameObject[] waterObstacles = new GameObject[1];
+    private List<float> waterDirections = new List<float>();
+    private List<GameObject> spawnedWaterObj = new List<GameObject>();
+    private GameObject[] waterSpawns = new GameObject[0];
+
+    private float minTime = .5f;
+    private float maxTime = 2f;
+    private float gameEdge = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        CheckForSpawns();
+    }
+
+    private void Update()
+    {
+        ObstacleMovement();
+    }
+
+    private void CheckForSpawns()
+    {
+        rdSpawns = GameObject.FindGameObjectsWithTag("ObsSpwnRd");
+        waterSpawns = GameObject.FindGameObjectsWithTag("WaterSpwnPnt");
+
+        DeleteFarAway();
+
+        //Debug.Log("checking now");
+        rdSpawns = GameObject.FindGameObjectsWithTag("ObsSpwnRd"); //Checks for what road spawn points are available
+        waterSpawns = GameObject.FindGameObjectsWithTag("WaterSpwnPnt"); //Checks for what water spawn points are available
+
+        //The rest spawns obstacles from currently existing spawn points
+        //Does so at staggered times
+        for (int x = 0; x < rdSpawns.Length; x++)
+        {
+            Vector3 pos = rdSpawns[x].transform.position;
+            StartCoroutine(SpawnRdObs(pos));
+        }
+
+        for (int x = 0; x < waterSpawns.Length; x++)
+        {
+            Vector3 pos = waterSpawns[x].transform.position;
+            StartCoroutine(SpawnWaterObj(pos));
+        }
+
+        Invoke("CheckForSpawns", maxTime);
+    }
+
+    //Spawns objects and sets their direction based on spawn pos
+    IEnumerator SpawnRdObs(Vector3 pos)
+    {
+        float waitTime = Random.Range(minTime, maxTime);
+        yield return new WaitForSeconds(waitTime);
+
+        int toSpawn = Random.Range(0, roadObstacles.Length);
+
+        Vector3 rotation = Vector3.zero;
+        float direction = 0;
+        //going left
+        if (pos.x > 0)
+        {
+            direction = -5f;
+            rotation = new Vector3(0, 270, 0);
+        }
+        //going right
+        else if (pos.x < 0)
+        {
+            direction = 5f;
+            rotation = new Vector3(0, 90, 0);
+        }
+        //Uh oh
+        else
+        {
+            Debug.Log("Man, what did you do now?");
+        }
+        //Spawns the objects and sets their rotation and direction
+        GameObject newObj = Instantiate(roadObstacles[toSpawn], pos, Quaternion.identity);
+        newObj.transform.localEulerAngles = rotation;
+        spawnedRdObj.Add(newObj);
+        rdDirections.Add(direction);
+    }
+
+    //For Spawning water objects
+    IEnumerator SpawnWaterObj(Vector3 pos)
+    {
+        float waitTime = Random.Range(minTime, maxTime);
+        yield return new WaitForSeconds(waitTime);
+
+        int toSpawn = Random.Range(0, waterObstacles.Length);
+
+        Vector3 rotation = Vector3.zero;
+        float direction = 0;
+        //going left
+        if (pos.x > 0)
+        {
+            direction = -3f;
+            rotation = new Vector3(0, 270, 0);
+        }
+        //going right
+        else if (pos.x < 0)
+        {
+            direction = 3f;
+            rotation = new Vector3(0, 90, 0);
+        }
+        //Uh oh
+        else
+        {
+            Debug.Log("Man, what did you do now?");
+        }
+        //Spawns the objects and sets their rotation and direction
+        GameObject newObj = Instantiate(waterObstacles[toSpawn], pos, Quaternion.identity);
+        newObj.transform.localEulerAngles = rotation;
+        spawnedWaterObj.Add(newObj);
+        waterDirections.Add(direction);
+    }
+
+    //Will need to incorporate pooling later
+    private void ObstacleMovement()
+    {
+        int toDeleteRd = -1;
+        int toDeleteWater = -1;
+        for (int x = 0; x < spawnedRdObj.Count; x++)
+        {
+            float currentX = spawnedRdObj[x].transform.position.x;
+            Vector3 toMove = Vector3.zero;
+            toMove.x = rdDirections[x] * Time.deltaTime;
+            spawnedRdObj[x].transform.position += toMove;
+            if (currentX > gameEdge || currentX < -gameEdge)
+            {
+                toDeleteRd = x;
+            }
+        }
+        for (int x = 0; x < spawnedWaterObj.Count; x++)
+        {
+            float currentX = spawnedWaterObj[x].transform.position.x;
+            Vector3 toMove = Vector3.zero;
+            toMove.x = waterDirections[x] * Time.deltaTime;
+            spawnedWaterObj[x].transform.position += toMove;
+            if (currentX > gameEdge || currentX < -gameEdge)
+            {
+                toDeleteWater = x;
+            }
+        }
+        //If an object is out of bounds will delete it, need to rework to object pool instead
+        if (toDeleteRd > -1)
+        {
+            Destroy(spawnedRdObj[toDeleteRd]);
+            spawnedRdObj.RemoveAt(toDeleteRd);
+            rdDirections.RemoveAt(toDeleteRd);
+        }
+        if (toDeleteWater > -1)
+        {
+            Destroy(spawnedWaterObj[toDeleteWater]);
+            spawnedWaterObj.RemoveAt(toDeleteWater);
+            waterDirections.RemoveAt(toDeleteWater);
+        }
+    }
+
+    //The player is gonna use this to find the direction to go on a log
+    public float HandOverThatFloat(Collider other)
+    {
+        float theChosenOne = 0f;
+        int thisOne = 0;
+
+        for (int x = 0; x < spawnedWaterObj.Count; x++)
+        {
+            if (other.transform == spawnedWaterObj[x].transform)
+            {
+                thisOne = x;
+            }
+        }
+        theChosenOne = waterDirections[thisOne];
+        return (theChosenOne);
+    }
+
+    //shows how far away player is from spawns
+    private float ComparedDist(GameObject toCompare)
+    {
+        float dist = GridMoveScript.instance.transform.position.z - toCompare.transform.position.z;
+
+        return (dist);
+    }
+
+    //any spawns more than 5 units away are deleted
+    private void DeleteFarAway()
+    {
+        if (rdSpawns.Length > 1)
+        {
+            for (int x = 0; x < rdSpawns.Length; x++)
+            {
+                float dist = ComparedDist(rdSpawns[x]);
+                if (dist > 5)
+                {
+                    Destroy(rdSpawns[x]);
+                }
+            }
+        }
+
+        if (waterSpawns.Length > 1)
+        {
+            for (int x = 0; x < waterSpawns.Length; x++)
+            {
+                float dist = ComparedDist(waterSpawns[x]);
+                if (dist > 5)
+                {
+                    Destroy(waterSpawns[x]);
+                }
+            }
+        }
+
+    }
+
+}
---
+++ JumpyStreetProject/Assets/Scripts/PlayerControlScript.cs
@@ -0,0 +1,267 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControlScript : MonoBehaviour
+{
+    public static PlayerControlScript instance;
+    public GameObject[] playerModels = new GameObject[4];
+
+    //gets relative directions
+    private Vector3 up = Vector3.zero,
+        right = new Vector3(0, 90, 0),
+        down = new Vector3(0, 180, 0),
+        left = new Vector3(0, 270, 0),
+        currentDir = Vector3.zero;
+
+    //to be filled in to see which way to face and which way to go
+    private Vector3 nextPos, destination, diection;
+
+    private float speed = 4f;
+    private float rayLength = 1f;
+    private float inheritedSpeed = 0f;
+
+    [HideInInspector]
+    public float score;
+
+    private bool canMove;
+    private bool alive;
+
+    [HideInInspector]
+    public bool onLand;
+    [HideInInspector]
+    public bool onLog;
+
+    [HideInInspector]
+    public int modelChoice;
+
+    void Start()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        modelChoice = 0;
+        alive = true;
+        onLand = true;
+        onLog = false;
+
+        currentDir = up;
+        nextPos = Vector3.forward;
+        destination = transform.position;
+
+        HidePlayerModels();
+        ChooseModel(); //need to only do this once we have the proper value set
+    }
+
+    void Update()
+    {
+        if (!onLand && !onLog)
+        {
+            float roundedZ = Mathf.Round(transform.position.z);
+
+            if (roundedZ == transform.position.z)
+            {
+                Death("splash");
+            }
+        }
+        if (alive)
+        {
+            Move();
+        }
+        if (onLog)
+        {
+            LogMove();
+        }
+    }
+
+    private void HidePlayerModels()
+    {
+        for (int x = 0; x < playerModels.Length; x++)
+        {
+            playerModels[x].SetActive(false);
+        }
+    }
+
+    private void ChooseModel()
+    {
+        playerModels[modelChoice].SetActive(true);
+    }
+
+    //Handles where you gonna move
+    private void Move()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+
+        GetInputs();
+
+        if (Vector3.Distance(destination, transform.position) <= .0001f)
+        {
+            transform.localEulerAngles = currentDir;
+            //If are not stuck and can move to a spot will move you
+            if (canMove && Valid())
+            {
+                //Only corrects your horizontal placement if you are moving up our down
+                //This allows us to avoid needing nodes to place player on while keeping
+                //the player from crashing into a wall they are halfway through
+                float roundedZ = Mathf.Round(transform.position.z);
+
+                if (roundedZ != transform.position.z)
+                {
+                    destination = transform.position + nextPos;
+                    destination.x = Mathf.Round(destination.x);
+                    //Debug.Log(destination.x);
+                    canMove = false;
+                    score = getScore();
+                }
+                else
+                {
+                    destination = transform.position + nextPos;
+
+                    canMove = false;
+                    score = getScore();
+                }
+            }
+
+        }
+    }
+
+    private float getScore()
+    {
+        float score = 0;
+        score = transform.position.z;
+        return (score);
+    }
+
+    private void LogMove()
+    {
+        float roundedZ = Mathf.Round(transform.position.z);
+
+        if (roundedZ == transform.position.z)
+        {
+            Vector3 toMove = Vector3.zero;
+            toMove.x = inheritedSpeed * Time.deltaTime;
+            destination += toMove;
+            //Debug.Log("I have moved " + toMove.x + " Since last time");
+
+        }
+        else
+        {
+            //Debug.Log("I am moving up and down, don't bug me");
+        }
+    }
+
+    //Gets your inputs
+    private void GetInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            nextPos = Vector3.forward;
+            currentDir = up;
+            canMove = true;
+        }
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            nextPos = Vector3.back;
+            currentDir = down;
+            canMove = true;
+        }
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            nextPos = Vector3.right;
+            currentDir = right;
+            canMove = true;
+        }
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            nextPos = Vector3.left;
+            currentDir = left;
+            canMove = true;
+        }
+    }
+
+    //Checks to see if you can move there
+    private bool Valid()
+    {
+        Ray myRay = new Ray(transform.position + new Vector3(0, .25f, 0), transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(myRay.origin, myRay.direction, Color.red);
+
+        if (Physics.Raycast(myRay, out hit, rayLength))
+        {
+            if (hit.collider.tag == "Wall")
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //Collider, use this when hitting an obstacle
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log("Hey I'm colliding over hea!");
+
+        if (other.gameObject.tag == "Killer")
+        {
+            Death("splat");
+        }
+        if (other.gameObject.tag == "Land")
+        {
+            onLand = true;
+        }
+        if (other.gameObject.tag == "Floater")
+        {
+            if (!onLog)
+            {
+                inheritedSpeed = ObstacleSpawnerScript.instance.HandOverThatFloat(other);
+                Debug.Log("The inherited speed should be " + inheritedSpeed);
+            }
+            onLog = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Land")
+        {
+            onLand = false;
+        }
+        if (other.gameObject.tag == "Floater")
+        {
+            onLog = false;
+        }
+    }
+
+    //Everything that happens in death should occur here
+    private void Death(string death)
+    {
+        Debug.Log("you ded man");
+        switch (death)
+        {
+            case "splat":
+                this.transform.localScale = new Vector3(1.5f, .3f, 1.5f);
+                break;
+            case "splash":
+                nextPos = Vector3.down;
+                destination = transform.position + nextPos;
+                canMove = false;
+                transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
+                break;
+            case "outtaBounds":
+
+                break;
+            default:
+                Debug.Log("hey, how'd you end up dead anyway?");
+                break;
+        }
+        alive = false;
+    }
+
+}
